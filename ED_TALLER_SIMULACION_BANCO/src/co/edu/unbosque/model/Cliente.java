dc824562595e9ab453d5a7ea150c68c4a5125498
Manuel <Manuel@Manuel>
2023-05-20 11:34:45 -0500

    Prueba 11:35

--- ED_TALLER_SIMULACION_BANCO/src/co/edu/unbosque/model/Cliente.java
+++ ED_TALLER_SIMULACION_BANCO/src/co/edu/unbosque/model/Cliente.java
@@ -1,5 +1,27 @@
 package co.edu.unbosque.model;
 
 public class Cliente {
+	
+	public class Cliente {
+		private String nombre;
+		private Cliente siguiente;
+		
+
+		public Cliente(String nombre) {
+			this.nombre = nombre;
+			this.siguiente = null;
+		}
+
+		public String getNombre() {
+			return nombre;
+		}
+
+		public Cliente getSiguiente() {
+			return siguiente;
+		}
+
+		public void setSiguiente(Cliente siguiente) {
+			this.siguiente = siguiente;
+		}
 
 }
